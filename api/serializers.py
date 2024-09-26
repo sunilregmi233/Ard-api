850f018f51b1fe951fde3792b001f917966ede49
sunilregmi233 <sunilregmi223@gmail.com>
2024-09-26 13:15:16 +0545

    fix api post data

--- api/serializers.py
+++ api/serializers.py
@@ -9,11 +9,19 @@ class SensorSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'sensor_id']
 
 class SensorDataSerializer(serializers.ModelSerializer):
+    sensor_id = serializers.CharField(write_only=True)  # Declare sensor_id here for incoming data
+
     class Meta:
         model = SensorData
-        fields = ['sensor', 'temperature', 'humidity', 'timestamp']  # Include sensor_id
+        fields = ['sensor_id', 'temperature', 'humidity', 'timestamp']  # Include sensor_id
 
     def create(self, validated_data):
         sensor_id = validated_data.pop('sensor_id')
-        sensor = get_object_or_404(Sensor, sensor_id=sensor_id)
+        try:
+            # Retrieve the sensor based on the sensor_id
+            sensor = Sensor.objects.get(sensor_id=sensor_id)
+        except Sensor.DoesNotExist:
+            raise serializers.ValidationError({'sensor_id': 'Sensor with this ID does not exist.'})
+
+        # Create SensorData instance with the retrieved sensor
         return SensorData.objects.create(sensor=sensor, **validated_data)
